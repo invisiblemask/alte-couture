a83adee7966a2b3e0e11a7e09ed9f29d524579e3
Kelechi Okeke <48678691+invisiblemask@users.noreply.github.com>
2022-03-09 12:26:24 +0100

    changes in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 const FooterContainer = styled.div`
-    display: grid;
-    place-items: center;
-    margin: 5rem 0 0;
+  display: grid;
+  place-items: center;
+  margin: 5rem 0 0;
 `
 
 const FooterText = styled.div`
-    text-align: center;
-    margin: 0;
-    color: #6a6a6a;
+  text-align: center;
+  margin: 0;
+  color: #6a6a6a;
 `
 
 export const Footer = () => {
-    const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
 
   return (
     <FooterContainer>
